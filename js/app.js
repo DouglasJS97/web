acef6001f3a5b33ab5779fdfe8c2355e33e1e015
DouglasJS97 <69151473+DouglasJS97@users.noreply.github.com>
2021-07-06 16:10:05 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,83 +1,27 @@
-class Lista{
-	
-	constructor(){
-		this.arrayProduto = []
-		this.id = 1
-	}
-	
-	salvar(){
-		let produto = {}
-		this.pegarDados(produto)
-		if(this.validaDados(produto) == true){
-			this.adicionar(produto)
-		}
-		this.listarProduto(produto)
-		this.limpar()
-	}
-	
-	cancela(){
-		this.limpar()
-	}
-	
-	listarProduto(){
-		let tbody = document.getElementById('tbody')
-		tbody.innerText = ''
-		for(let i = 0;i < this.arrayProduto.length;i++){
-			let tr = tbody.insertRow()
-			
-			//let td_id = tr.insertCell()
-			let td_descricao = tr.insertCell()
-			let td_qtd = tr.insertCell()
-			let td_preco = tr.insertCell()
-			let td_total = tr.insertCell()
-			//let td_acao = tr.insertCell()
-			
-			//td_id.innerText = this.arrayProduto[i].id
-			td_descricao.innerText = this.arrayProduto[i].descricao
-			td_qtd.innerText = this.arrayProduto[i].qtd
-			td_preco.innerText = this.arrayProduto[i].preco
-			//td_acao.innerText = this.arrayProduto[i].acao
-			//td_total.innerText = this.arrayProduto[i].qtd * this.arrayProduto[i].preco
-		}
-	}
-	
-	adicionar(produto){
-		this.arrayProduto.push(produto)
-		this.id++
-	}
-	
-	pegarDados(getProduto){
-		getProduto.id = this.id
-		getProduto.descricao = document.getElementById('descricao').value
-		getProduto.qtd = document.getElementById('qtd').value
-		getProduto.preco = document.getElementById('preco').value
-		getProduto.preco = parseFloat(getProduto.preco)
-		getProduto.qtd = parseFloat(getProduto.qtd)
-		
-		return
-	}
-	
-	validaDados(produto){
-		let msg = ''
-		if(produto.descricao == ''){
-			msg += '** Descrição inválida **\n- Informe a descrição do produto!'
-		}
-		
-		if(msg != ''){
-			alert(msg)
-			return false
-		}
-		
-		return true
-	}
-	
-	limpar(){
-		document.getElementById('descricao').value = ''
-		document.getElementById('qtd').value = ''
-		document.getElementById('preco').value = ''
-	}
-	
-	
+function padrao(){
+    let ul = document.querySelector('ul');
+    let nav = document.querySelector('nav');
+    if(window.innerWidth <= 375){
+        ul.style.visibility = 'hidden';
+        nav.style.height = '0';
+    }else{
+        ul.style.visibility = 'visible';
+    }
 }
 
-let lista = new Lista()
+function menu(){
+    let ul = document.querySelector('ul');
+    let nav = document.querySelector('nav');
+    
+    if(window.innerWidth <= 375){
+        if(ul.style.visibility == 'hidden'){
+            ul.style.visibility = 'visible';
+            nav.style.height = '130px';
+        }else{
+            ul.style.visibility = 'hidden';
+            nav.style.height = '0';
+        }
+    }else{
+        ul.style.visibility = 'visible';
+    }
+}
